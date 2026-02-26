176af2bd2b109ca00db659b15776e1abd35db5c3
Tim Ipema <tim@indifferentbroccoli.com>
2026-02-26 09:44:32 -0600

    Doorstop path

--- scripts/start.sh
+++ scripts/start.sh
@@ -86,7 +86,7 @@ if [ "${BEPINEX_ENABLED}" = true ]; then
     export DOORSTOP_INVOKE_DLL_PATH="./BepInEx/core/BepInEx.Preloader.dll"
     export DOORSTOP_CORLIB_OVERRIDE_PATH="./unstripped_corlib"
     export LD_LIBRARY_PATH="./doorstop_libs:$LD_LIBRARY_PATH"
-    export LD_PRELOAD="libdoorstop_x64.so:$LD_PRELOAD"
+    export LD_PRELOAD="./doorstop_libs/libdoorstop_x64.so:$LD_PRELOAD"
     export LD_LIBRARY_PATH="./linux64:$LD_LIBRARY_PATH"
 fi
 
